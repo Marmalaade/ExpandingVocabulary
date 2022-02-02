a567c191bb8238307d41a922c1325abf93f26bd8
Marmalaade <79687328+Marmalaade@users.noreply.github.com>
2022-02-02 23:40:16 +0300

    add fragments+databinding+navigation

---
+++ app/src/main/java/com/example/expandingvocabulary/tittle/TittleFragment.kt
@@ -0,0 +1,27 @@
+package com.example.expandingvocabulary.tittle
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import com.example.expandingvocabulary.R
+import com.example.expandingvocabulary.databinding.FragmentTittleBinding
+
+
+class TittleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentTittleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_tittle, container, false)
+        binding.playGameButton.setOnClickListener {
+            findNavController().navigate(TittleFragmentDirections.actionTittleFragmentToGameFragment())
+        }
+        return binding.root
+    }
+
+}
